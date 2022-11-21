4466057b416af8d6fee608f7e5bf8af96e047375
Michael Arrabito <mikearrabito@gmail.com>
2022-11-20 22:45:52 -0500

    init

---
+++ index.ts
@@ -0,0 +1,195 @@
+import { Queue, QueueNode } from "./types/Queue";
+const customInspectSymbol = Symbol.for("nodejs.util.inspect.custom");
+
+class QueueImpl<T> implements Queue<T> {
+  private head: QueueNode<T> | null = null;
+  private tail: QueueNode<T> | null = null;
+
+  constructor(...initData: T[]) {
+    let prev: QueueNode<T> | null = null;
+
+    for (let i = 0; i < initData.length; i++) {
+      const newNode = new QueueNodeImpl(initData[i]);
+      newNode.prev = prev;
+      if (i === 0) {
+        this.head = newNode;
+      }
+      if (i > 0) {
+        // null check not needed since i=0 iter should set
+        (prev as QueueNode<T>).next = newNode;
+      }
+      prev = newNode;
+    }
+
+    this.tail = prev;
+  }
+
+  front() {
+    return this.getHead();
+  }
+
+  back() {
+    return this.getTail();
+  }
+
+  enqueue(data: T) {
+    return this.pushFront(data);
+  }
+
+  dequeue() {
+    return this.popBack();
+  }
+
+  getHead() {
+    return this.head?.data ?? null;
+  }
+
+  getTail() {
+    return this.tail?.data ?? null;
+  }
+
+  pushFront(data: T) {
+    const newNode = new QueueNodeImpl(data);
+    newNode.next = this.head;
+    if (this.head !== null) {
+      this.head.prev = newNode;
+    }
+    this.head = newNode;
+    if (this.tail === null) {
+      this.tail = newNode;
+    }
+  }
+
+  pushBack(data: T) {
+    const newNode = new QueueNodeImpl(data);
+    newNode.prev = this.tail;
+    if (this.tail !== null) {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    if (this.head === null) {
+      this.head = newNode;
+    }
+  }
+
+  popFront() {
+    const head = this.head;
+    if (head === null) {
+      return null;
+    }
+
+    const nextNode = head.next;
+
+    if (nextNode !== null) {
+      nextNode.prev = null;
+      this.head = nextNode;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    const data = head.data;
+    head.clear();
+    return data;
+  }
+
+  popBack() {
+    const tail = this.tail;
+    if (tail === null) {
+      return null;
+    }
+
+    const prevNode = tail.prev;
+
+    if (prevNode !== null) {
+      prevNode.next = null;
+      this.tail = prevNode;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    const data = tail.data;
+    tail.clear();
+    return data;
+  }
+
+  clear() {
+    let cur = this.head;
+    let prev = this.head;
+    while (cur !== null) {
+      cur = cur.next;
+      prev?.clear();
+      prev = cur;
+    }
+    this.head = null;
+    this.tail = null;
+  }
+
+  toString() {
+    let queueStr = "Queue: [ ";
+    let cur = this.head;
+
+    if (cur === null) {
+      return "Queue: Queue is empty";
+    }
+
+    while (cur != null) {
+      const val = cur.data;
+      if (typeof val === "object" && val !== null) {
+        queueStr += "object, ";
+      } else {
+        queueStr += String(val) + ", ";
+      }
+      cur = cur.next;
+    }
+
+    return queueStr.substring(0, queueStr.length - 2) + " ]";
+  }
+
+  [customInspectSymbol]() {
+    return this.toString();
+  }
+}
+
+class QueueNodeImpl<T> implements QueueNode<T> {
+  private _prev: QueueNode<T> | null = null;
+  private _next: QueueNode<T> | null = null;
+  private _data: T | null;
+
+  constructor(data: T) {
+    this._data = data;
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  set data(newData: T | null) {
+    this._data = newData;
+  }
+
+  get prev() {
+    return this._prev;
+  }
+
+  set prev(newPrev: QueueNode<T> | null) {
+    this._prev = newPrev;
+  }
+
+  get next() {
+    return this._next;
+  }
+
+  set next(newNext: QueueNode<T> | null) {
+    this._next = newNext;
+  }
+
+  clear() {
+    this._prev = null;
+    this._next = null;
+    this._data = null;
+  }
+}
+
+export default QueueImpl;
---
+++ test/index.spec.ts
@@ -0,0 +1,132 @@
+import { describe } from "mocha";
+import { expect } from "chai";
+import Queue from "../index";
+import { Queue as QueueType } from "../types/Queue";
+
+let queue: QueueType<number> | null;
+
+beforeEach(() => {
+  queue = null;
+});
+
+const genRandomNumbers = () => {
+  return Array.from({ length: 10 }, () => Math.floor(Math.random() * 1000));
+};
+
+describe("Queue Tests", () => {
+  it("should initialize an empty queue", () => {
+    queue = new Queue();
+    expect(queue).to.not.be.null;
+  });
+  it("should initialize queue with list of elements", () => {
+    queue = new Queue(...genRandomNumbers());
+  });
+  it("should enqueue and dequeue in correct order", () => {
+    queue = new Queue();
+    const randomNums = genRandomNumbers();
+    for (const num of randomNums) {
+      queue.enqueue(num);
+    }
+    for (let i = 0; i < randomNums.length; i++) {
+      expect(queue.dequeue()).to.equal(randomNums[i]);
+    }
+    expect(queue.getHead()).to.be.null;
+    expect(queue.getTail()).to.be.null;
+  });
+  it("should pop correct value from front", () => {
+    const randomNums = genRandomNumbers();
+    queue = new Queue(...randomNums);
+    expect(queue.popFront()).to.equal(randomNums[0]);
+  });
+  it("should pop correct value from back", () => {
+    const randomNums = genRandomNumbers();
+    queue = new Queue(...randomNums);
+    expect(queue.popBack()).to.equal(randomNums[randomNums.length - 1]);
+  });
+  it("should push value to front", () => {
+    const randomNums = genRandomNumbers();
+    queue = new Queue(...randomNums);
+    const randomNum = Math.random();
+    queue.pushFront(randomNum);
+    expect(queue.front()).to.equal(randomNum);
+  });
+  it("should push value to back", () => {
+    const randomNums = genRandomNumbers();
+    queue = new Queue(...randomNums);
+    const randomNum = Math.random();
+    queue.pushBack(randomNum);
+    expect(queue.back()).to.equal(randomNum);
+  });
+  it("should push value to front and pop from front correctly", () => {
+    const randomNums = genRandomNumbers();
+    queue = new Queue(...randomNums);
+    const randomNum = Math.random();
+    queue.pushFront(randomNum);
+    expect(queue.popFront()).to.equal(randomNum);
+    expect(queue.front()).to.equal(randomNums[0]);
+  });
+  it("should push value to back and pop from back correctly", () => {
+    const randomNums = genRandomNumbers();
+    queue = new Queue(...randomNums);
+    const randomNum = Math.random();
+    queue.pushBack(randomNum);
+    expect(queue.popBack()).to.equal(randomNum);
+    expect(queue.back()).to.equal(randomNums[randomNums.length - 1]);
+  });
+  it("should clear queue", () => {
+    queue = new Queue(...genRandomNumbers());
+    queue.clear();
+    expect(queue.front()).to.be.null;
+    expect(queue.back()).to.be.null;
+    expect(queue.popFront()).to.be.null;
+    expect(queue.popBack()).to.be.null;
+  });
+  it("should allow insertion after clearing", () => {
+    queue = new Queue(...genRandomNumbers());
+    queue.clear();
+    expect(queue.front()).to.be.null;
+    expect(queue.back()).to.be.null;
+    expect(queue.popFront()).to.be.null;
+    expect(queue.popBack()).to.be.null;
+
+    const randomNum = Math.random();
+    queue.pushBack(randomNum);
+    expect(queue.front()).to.equal(randomNum);
+    expect(queue.back()).to.equal(randomNum);
+
+    const secondRandomNum = Math.random();
+    queue.pushBack(secondRandomNum);
+    expect(queue.front()).to.equal(randomNum);
+    expect(queue.back()).to.equal(secondRandomNum);
+  });
+  it("should update pointers correctly for queue of size 1", () => {
+    const initNums = genRandomNumbers().slice(0, 2);
+    // init with 2 values
+    queue = new Queue(...initNums);
+
+    expect(queue.front()).to.equal(initNums[0]);
+    expect(queue.back()).to.equal(initNums[1]);
+
+    expect(queue.popBack()).to.equal(initNums[1]);
+
+    // queue size 1, head and tail should be same
+    expect(queue.front()).to.equal(initNums[0]);
+    expect(queue.back()).to.equal(initNums[0]);
+
+    const randomNum = Math.random();
+    queue.pushBack(randomNum);
+    expect(queue.front()).to.equal(initNums[0]);
+    expect(queue.back()).to.equal(randomNum);
+  });
+  it("should print toString correctly", () => {
+    queue = new Queue();
+    expect(queue.toString()).to.contain("empty");
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    expect(queue.toString())
+      .to.contain("1")
+      .and.to.contain("2")
+      .and.to.contain("3");
+  });
+});
